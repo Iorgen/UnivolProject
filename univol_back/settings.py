e790905342bd123708098c721bdd1fe3d12dafaa
Vyacheslav <sokdestroy@gmail.com>
2019-09-13 23:40:01 +0700

    settings

--- univol_back/settings.py
+++ univol_back/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '407p3yt1pe4!v4k-ipoqu)kp(b)pcx8ohqo1#@z!qzzy7o=lz2'
 
 # # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'safe-bastion-88967.herokuapp.com',
     '127.0.0.1'
 ]
-DEBUG = True
+
 
 # Application definition
 INSTALLED_APPS = [
