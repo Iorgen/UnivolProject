220146de84020a32aec69c9f0bdbbb8d80eed559
Vyacheslav <sokdestroy@gmail.com>
2019-09-14 00:19:45 +0700

    using CustomUser

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+from users.models import CustomUser
 from .models import Profile
 
 
@@ -8,7 +9,7 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
 
 
@@ -16,7 +17,7 @@ class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email']
 
 
