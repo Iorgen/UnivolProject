7989a0c72c2c35ff9edb8339fca87c1e88b990bc
Vyacheslav <sokdestroy@gmail.com>
2019-09-13 15:42:58 +0700

    fix models

--- univol/models.py
+++ univol/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import Organization
+
 
 class Contacts(models.Model):
     phone_number = models.TextField(max_length=40)
@@ -9,3 +11,12 @@ class Contacts(models.Model):
     facebook_link = models.TextField(max_length=100)
     vk_link = models.TextField(max_length=100)
     email_adress = models.TextField(max_length=100)
+
+
+class Vacancy(models.Model):
+    vacancy_name = models.TextField(max_length=50)
+    date_range = models.TextField(max_length=21)
+    description = models.TextField(max_length=5000)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    competitions_keywords = models.TextField(max_length=5000)
+    event_name = models.TextField(max_length=1000)
--- users/models.py
+++ users/models.py
@@ -26,17 +26,3 @@ class Volunteer(models.Model):
     rating = models.FloatField(default=0)
     description = models.TextField(max_length=5000)
     employment_situation = models.TextField(max_length=100)
-
-
-class Vacancy(models.Model):
-    vacancy_name = models.TextField(max_length=50)
-    date_range = models.TextField(max_length=21)
-    description = models.TextField(max_length=5000)
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    competitions_keywords = models.TextField(max_length=5000)
-    event_name = models.TextField(max_length=1000)
-
-
-
-
-
